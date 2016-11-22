be3ae5a7d5f1e94f1f0abdc4a7c1a2141d282d49
zaki1993 <zaki1993@abv.bg>
2016-11-22 03:49:45 -0800

    Check if x is even

--- Homework2/Task3.java
+++ Homework2/Task3.java
@@ -5,7 +5,9 @@ public class Task3 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		for(int i = -10;i<=10;i++){
-			System.out.println(i);
+			if(x % 2 != 0){
+				System.out.println(i);
+			}
 		}
 	}
 
