111f87a0c1fcb1bf916dabd9d7cdae74a2da94fb
zaki1993 <zaki1993@abv.bg>
2016-12-10 13:43:08 -0800

    Update Task3.java

--- ITTalents/Homework5/Task3.java
+++ ITTalents/Homework5/Task3.java
@@ -5,19 +5,38 @@ public class Task3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String wordOne, wordTwo;
-		
+		boolean different = false;
 		do{
 			System.out.println("Enter a word: ");
 			wordOne = sc.next();
 			System.out.println("Enter an another word: ");
 			wordTwo = sc.next();			
-		}while(wordOne.contains(" ") || wordTwo.contains(" ") || wordOne.length() != wordTwo.length());
+		}while(wordOne.contains(" ") || wordTwo.contains(" "));
 		
-		for (int i = 0; i < wordOne.length(); i++) {
+		if(wordOne.length() != wordTwo.length()){
+			System.out.println("The words are with different length!");
+			different = true;
+		}
+		else{
+			System.out.println("The words are with same length!");
+		}
+		
+		int smallLength = (wordOne.length() < wordTwo.length()?wordOne.length():wordTwo.length());
+		
+		for (int i = 0; i < smallLength; i++) {
 			if(wordOne.charAt(i)!= wordTwo.charAt(i)){
 				System.out.println(i + " : " + wordOne.charAt(i) + " - " + wordTwo.charAt(i));
 			}
 		}
+		
+		if(different){
+			int bigLength = (wordOne.length() > wordTwo.length()?wordOne.length():wordTwo.length());
+			char wordPrint = (bigLength == wordOne.length()?'1':'2');
+			for (int i = smallLength; i < bigLength; i++) {
+				System.out.println(i + " : " + (wordPrint == '1'?wordOne.charAt(i):wordTwo.charAt(i)) + " - " + " ");
+			}
+		}
+		
 	}
 
 }
