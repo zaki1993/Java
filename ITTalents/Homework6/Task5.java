25f7356a257d5c5f9b30fa735fdcdfa8f7dc80c1
zaki1993 <zaki1993@abv.bg>
2016-12-12 13:27:01 -0800

    Fix algorithm for Task5

--- ITTalents/Homework6/Task5.java
+++ ITTalents/Homework6/Task5.java
@@ -2,20 +2,29 @@
 public class Task5 {
 
 	public static void main(String[] args) {
-		System.out.println(palindrome(501105));
+		System.out.println(palindrome(504005));
 	}
 	
 	public static boolean palindrome(int number){
+		if(number < 0){
+			return palindrome(-1*number);
+		}
 		if(number < 10){
 			return true;
 		}
-		if(number%10 == 0 && (int)(number/(power(10,length(number)-1))) != 0){
-			return palindrome((number - (number%10*power(10,length(number) - 1) + number%10))/10);
-		}
-		if(number%10 != (int)(number/(power(10,length(number)-1)))){
+		if(number%10 != (number/(power(10,length(number) - 1)))){
 			return false;
 		}
-		return palindrome(number - (number%10*power(10,length(number) - 1) + number%10));
+		int dummy = (number - ((number%10*power(10,length(number) - 1)) + number%10))/10;
+		if(length(dummy) == 0){
+			return true;
+		}
+		else if(frontZeros(number) - (length(dummy) - nonZeros(dummy)) != 0){
+			return false;
+		}
+		else{
+			return palindrome((number - ((number%10*power(10,length(number) - 1)) + number%10))/power(10,length(number) - length(dummy) - 2 + nonZeros(dummy)));
+		}
 	}
 	
 	public static int length(int number){
@@ -31,6 +40,27 @@ public class Task5 {
 		int result = 1;
 		for (int i = 0; i < y;i++) {
 			result*=x;
+		}
+		return result;
+	}
+	
+	public static int nonZeros(int num){
+		int result = 0;
+		while(num!=0){
+			if(num%10 != 0){
+				result++;
+			}
+			num/=10;
+		}
+		return result;
+	}
+	
+	public static int frontZeros(int num){
+		int result = 0;
+		int counter = 2;
+		while(num/power(10, length(num) - counter)%10 == 0){
+			result++;
+			counter++;
 		}
 		return result;
 	}
