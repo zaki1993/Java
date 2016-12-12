16b0c15b5927d9e5878b85b85e656b730b02c2bb
zaki1993 <zaki1993@abv.bg>
2016-12-12 07:22:29 -0800

    Fix bug

--- ITTalents/Homework6/Task5.java
+++ ITTalents/Homework6/Task5.java
@@ -2,30 +2,20 @@
 public class Task5 {
 
 	public static void main(String[] args) {
-		System.out.println(palindrome(5));
+		System.out.println(palindrome(501105));
 	}
 	
 	public static boolean palindrome(int number){
 		if(number < 10){
 			return true;
 		}
-		if(length(number) != length(reverseNum(number))){
-			return palindrome(number/10);
+		if(number%10 == 0 && (int)(number/(power(10,length(number)-1))) != 0){
+			return palindrome((number - (number%10*power(10,length(number) - 1) + number%10))/10);
 		}
-		if(number%10 != reverseNum(number)%10){
+		if(number%10 != (int)(number/(power(10,length(number)-1)))){
 			return false;
 		}
 		return palindrome(number - (number%10*power(10,length(number) - 1) + number%10));
-	}
-	
-	public static int reverseNum(int number){
-		int result = 0;
-		while(number!=0){
-			result*=10;
-			result+=number%10;
-			number/=10;
-		}
-		return result;
 	}
 	
 	public static int length(int number){
