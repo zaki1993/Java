fdff8dafbf38837b5662b7f5e44fd98649f3a78b
zaki1993 <zaki1993@abv.bg>
2016-12-12 07:01:02 -0800

    Fix this

--- ITTalents/Homework6/Task5.java
+++ ITTalents/Homework6/Task5.java
@@ -2,11 +2,11 @@
 public class Task5 {
 
 	public static void main(String[] args) {
-		System.out.println(palindrome(10));
+		System.out.println(palindrome(5));
 	}
 	
 	public static boolean palindrome(int number){
-		if(number == 0){
+		if(number < 10){
 			return true;
 		}
 		if(length(number) != length(reverseNum(number))){
@@ -15,7 +15,7 @@ public class Task5 {
 		if(number%10 != reverseNum(number)%10){
 			return false;
 		}
-		return palindrome(number/10);
+		return palindrome(number - (number%10*power(10,length(number) - 1) + number%10));
 	}
 	
 	public static int reverseNum(int number){
@@ -33,6 +33,14 @@ public class Task5 {
 		while(number!=0){
 			result++;
 			number/=10;
+		}
+		return result;
+	}
+	
+	public static int power(int x, int y){
+		int result = 1;
+		for (int i = 0; i < y;i++) {
+			result*=x;
 		}
 		return result;
 	}
