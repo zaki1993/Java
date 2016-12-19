4b8d9e2fd999832ed95a6e3e2e017007f95c2190
zaki1993 <zaki1993@abv.bg>
2016-12-19 02:02:45 -0800

    Works with negative numers now
    If the minimal element is negative it will increase the number of elements instead of decreasing them

--- Sorting Methods/Count_sort.java
+++ Sorting Methods/Count_sort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Count_sort {
 
 	public static void main(String[] args) {
-		int[] arr = {8,4,7,2,8,2,5,1,9,0,7};
+		int[] arr = {8,4,7,2,-3,8,2,5,1,9,0,7};
 		int[] arr1 = countingSort(arr);
 		System.out.println(Arrays.toString(arr1));
 	}
@@ -22,14 +22,14 @@ public class Count_sort {
 		int holder[] = new int[max - min + 1];
 		int result[] = new int[arr.length];
 		for (int i = 0; i < arr.length; i++) {
-			holder[arr[i]] += 1; 
+			holder[arr[i] - min] += 1; 
 		}
 		for (int i = 1; i < holder.length; i++) {
-			holder[i] +=holder[i-1]; 
+			holder[i] +=holder[i-1];
 		}
 		for (int i = 0; i < arr.length; i++) {
-			holder[arr[i]]--; 
-			int position = holder[arr[i]]; 
+			holder[arr[i] - min]--; 
+			int position = holder[arr[i] - min]; 
 			result[position] = arr[i];
 		}
 		return result;
