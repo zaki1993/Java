d739def3a5fc80b78c13153746a965c7283a8fce
zaki1993 <zaki1993@abv.bg>
2017-08-16 14:29:08 +0300

    Update quick_sort.java

--- Sorting Methods/quick_sort.java
+++ Sorting Methods/quick_sort.java
@@ -1,46 +1,41 @@
-import java.util.Arrays;
+private static void swap(String[] a, int i, int j) {
+        String temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 
+    int partition(String[] arr, int left, int right)
+    {
+        int startIdx = left;
+        String pivot = arr[right];
+        for(int i = left; i < right; ++i)
+        {
+            if(arr[i].compareTo(pivot) >= 0)
+            {
+                swap(arr, startIdx, i);
+                startIdx++;
+            }
+        }
+        swap(arr, right, startIdx);
+        return startIdx;
+    }
 
-public class quick_sort {
-	public static void main(String[] args) {
-		int[] arr = {9,4,1,-5,8,9,4,1,-5,8,9,4,1,-5,8,9,4,1,-5,8,9,4,1,-5,8,3};
-		System.out.println(Arrays.toString(arr));
-		quickSort(arr, 0, arr.length - 1);
-		System.out.println(Arrays.toString(arr));
-	}
-	
-	public static int partition(int arr[], int left, int right)
-	{	
-	      int idxStart = left;
-	      int tmp; //define swap variable
-	      int pivot = arr[right]; //get the pivot element   
-	      for (int i = left;i<right;i++) {
-	    	  if(arr[i] <= pivot){
-	    		  tmp = arr[i];
-		    	  arr[i] = arr[idxStart];
-		    	  arr[idxStart] = tmp;
-		    	  idxStart++;
-	    	  }
-	      }
-	      tmp = arr[right];
-	      arr[right] = arr[idxStart];
-	      arr[idxStart] = tmp;
-	      
-	      return idxStart; // we have swapped all the elements
-	}
-
-	public static void quickSort(int arr[], int left, int right) {
-	      if(left >= right){
-	      	    return;
-	      }
-	      int index = partition(arr, left, right);
-	      if (left < index - 1){
-	    	  	//call quicksort for the left of the pivot
-	            quickSort(arr, left, index - 1);
-	      }
-	      if (index < right){
-	    	  	//call quicksort for the right of the pivot
-	            quickSort(arr, index, right);
-	      }
-	}
-}
+    void quicksort(String[] arr, int left, int right)
+    {
+        if(left >= right)
+        {
+            return;
+        }
+        else
+        {
+            int pivotIdx = partition(arr, left, right - 1);
+            if(left < pivotIdx - 1)
+            {
+                quicksort(arr, left, pivotIdx);
+            }
+            if(pivotIdx < right)
+            {
+                quicksort(arr, pivotIdx + 1, right);
+            }
+        }
+    }
